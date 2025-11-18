4e377596db1a320a9e30e72260d9dd5b56e7cb7a
Aman Pathan <amanpathan3318@gmail.com>
2025-11-18 17:52:27 +0530

    updated

--- jaldhara-shop-backend/routes/dashboard.js
+++ jaldhara-shop-backend/routes/dashboard.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Dashboard = require("../models/Dashboard");
+const Dashboard = require("../models/dashboardModel");
 
 // GET dashboard
 router.get("/", async (req, res) => {
