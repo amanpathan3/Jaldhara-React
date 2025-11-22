b4e04c76e31ed6571508a42c8391a4a361d19c78
Aman Pathan <amanpathan3318@gmail.com>
2025-11-22 09:55:58 +0530

    updated

--- jaldhara-shop/src/pages/Home/HomePage.jsx
+++ jaldhara-shop/src/pages/Home/HomePage.jsx
@@ -7,26 +7,29 @@ export function HomePage() {
     return (
         <>
             <Header />
-            <div className="relative w-full h-[40vh] sm:h-[40vh] md:h-[37vh] mt-16">
+            <div className="relative w-full h-[40vh] sm:h-[40vh] md:h-[37vh] mt-16 shadow-[0_20px_40px_-10px_rgba(0,0,0,0.4)]">
                 <img
                     src={PlumbingImg}
                     alt="plumbing-img"
                     className="w-full h-full object-cover"
                 />
+
+                {/* Shadow inside image (optional) */}
+                <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-black/20 to-transparent"></div>
                 {/* Text Overlay */}
-                <div className="absolute inset-0 flex flex-col justify-center items-center text-white px-10 md:px-20">
-                    <h3 className="text-xl md:text-4xl font-bold mb-4 w-full">
+                <div className="absolute inset-0 flex flex-col justify-center items-center text-white px-10 md:px-20 text-center">
+                    <h3 className="text-xl md:text-4xl font-bold mb-4">
                         JALDHARA MACHINERY AND PLUMBING MATERIAL
                     </h3>
+
                     <p className="text-lg md:text-2xl mb-6">
                         The trusted partner for quality plumbing solutions
                     </p>
-                    <button className="px-6 py-3 bg-blue-600 hover:bg-blue-700 rounded-lg text-white font-semibold">
+                   <button className="px-6 py-3 bg-blue-600 hover:bg-blue-700 rounded-lg text-white font-semibold">
                         Explore Now
                     </button>
                 </div>
             </div>
-
             <Carts />
             <Footer />
         </>
