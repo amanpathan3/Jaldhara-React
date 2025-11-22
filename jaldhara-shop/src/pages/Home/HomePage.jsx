28b325f284ef0693fd9d27233ca04895b1cfc46b
Aman Pathan <amanpathan3318@gmail.com>
2025-11-22 10:05:43 +0530

    updated home page

--- jaldhara-shop/src/pages/Home/HomePage.jsx
+++ jaldhara-shop/src/pages/Home/HomePage.jsx
@@ -2,32 +2,36 @@ import { Header } from "../../component/Header";
 import PlumbingImg from "../../assets/images.jpg";
 import { Footer } from "../../component/Footer";
 import { Carts } from "./Carts";
-
+import { Droplet } from "lucide-react";
 export function HomePage() {
     return (
         <>
             <Header />
             <div className="relative w-full h-[35vh] sm:h-[35vh] md:h-[32vh] mt-16 shadow-[0_20px_40px_-10px_rgba(0,0,0,0.4)]">
+                
                 <img
                     src={PlumbingImg}
                     alt="plumbing-img"
                     className="w-full h-full object-cover"
                 />
 
-                {/* Shadow inside image (optional) */}
+                {/* Bottom Shadow */}
                 <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-black/20 to-transparent"></div>
-                {/* Text Overlay */}
+
+                {/* OVERLAY CONTENT */}
                 <div className="absolute inset-0 flex flex-col justify-center items-center text-white px-10 md:px-20 text-center">
+
+                    {/* Lucide Icon */}
+                    <Droplet className="w-12 h-12 text-blue-400 drop-shadow-2xl mb-3" />
+
                     <h3 className="text-xl md:text-4xl font-bold mb-4">
                         JALDHARA MACHINERY AND PLUMBING MATERIAL
                     </h3>
 
-                    <p className="text-lg md:text-2xl mb-6">
+                    <p className="text-lg md:text-2xl mb-2">
                         The trusted partner for quality plumbing solutions
                     </p>
-                   {/* <button className="px-6 py-3 bg-blue-600 hover:bg-blue-700 rounded-lg text-white font-semibold">
-                        Explore Now
-                    </button> */}
+
                 </div>
             </div>
             <Carts />
